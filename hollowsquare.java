96de346c4f6774fb4bead5f232d1c31753becc95
Dharshini akb <dharshiniakb.23it@kongu.edu>
2026-08-27 11:20:56 +0530

    first commit

---
+++ hollowsquare.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[])
+    {
+        Scanner sc =new Scanner(System.in);
+        int row=sc.nextInt();
+        int column=sc.nextInt();
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<column;j++)
+            {
+                if(i==0||i==row-1||j==0||j==column-1)
+                {
+                System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
